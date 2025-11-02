3f3921fdebffcd71acd6e0ad0dae6006676f4f6e
KONDURI SRINIVAS <srinukonduri7703@gmail.com>
2025-11-02 15:20:19 +0530

    Test DB

---
+++ AutomapperExample/Migrations/20251102094620_Mig1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutomapperExample.Migrations
+{
+    /// <inheritdoc />
+    public partial class Mig1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2025, 11, 2, 15, 16, 19, 555, DateTimeKind.Local).AddTicks(18));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2025, 11, 2, 15, 9, 44, 14, DateTimeKind.Local).AddTicks(2016));
+        }
+    }
+}
--- AutomapperExample/Program.cs
+++ AutomapperExample/Program.cs
@@ -1,4 +1,7 @@
 
+using AutomapperExample.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace AutomapperExample
 {
     public class Program
@@ -9,7 +12,18 @@ namespace AutomapperExample
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers()
+               // Optionally, configure JSON options or other formatter settings
+               .AddJsonOptions(options =>
+               {
+                   // Configure JSON serializer settings to keep the Original names in serialization and deserialization
+                   options.JsonSerializerOptions.PropertyNamingPolicy = null;
+               });
+
+            builder.Services.AddAutoMapper(typeof(Program).Assembly);
+            // Register the ProductDbContext with dependency injection
+            builder.Services.AddDbContext<ECommerceDBContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("ECommerceDBConnection")));
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
